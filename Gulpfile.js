a708564e12310f3ca12af6031b07014d05ec9d93
Eleven Chen <skyhacker@126.com>
2014-12-15 10:34:05 +0800

    build 1.0

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,8 @@ var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
 var del = require('del')
 var copy = require('gulp-copy')
+var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
 
 var paths = {
 	scripts: ['coffee/**/*.coffee']
@@ -21,6 +23,17 @@ gulp.task('scripts', ['clean'], function() {
 gulp.task('copy',['clean'], function() {
 	return gulp.src("libs/**/*.js")
 		.pipe(gulp.dest("./src/libs/"))
+});
+
+gulp.task('cleanPublish', function(cb) {
+	del(['publish'], cb);
+});
+
+gulp.task('publish', ['cleanPublish'], function() {
+	return gulp.src(['frameworks/cocos2d-html5/**/*.js', 'src/**/*.js', 'main.js'])
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('publish'))
 });
 
 gulp.task('default', ['scripts', 'copy']);
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	path = require('path');
 
 var app = express();
-app.use(express.static(path.join(__dirname, './')));
+app.use(express.static(path.join(__dirname, './publish/html5')));
 
 var server = app.listen(process.env.VCAP_APP_PORT || 3000, function() {
     console.log('Listening on port %d', server.address().port);
